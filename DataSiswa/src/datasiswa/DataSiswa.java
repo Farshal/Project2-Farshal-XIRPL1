446e6276b65ed573d76a3bdde28c16f3de744670
Farshal <fyuzka@gmail.com>
2021-06-08 14:33:24 +0700

    Create Database dan Show Data

---
+++ DataSiswa/src/datasiswa/DataSiswa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datasiswa;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Farshal-Thinkpad
+ */
+public class DataSiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public JTable tbl_siswa;
+    // End of variables declaration
+    
+}
